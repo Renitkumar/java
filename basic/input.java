3ae9da722bafd8966935839ac1ebd99c4cd5dd0d
Renitkumar <renitkumar11@gmail.com>
2024-12-17 12:21:29 +0530

    Add files via upload

---
+++ basic/input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("plese enter no: ");
+        int a=input.nextInt();
+        System.out.println("a= "+a);
+    }
+}
---
+++ basic/sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    public static void main(String[]arr){
+        int a=4;
+        int b;
+        b=5;
+        int c=a+b;
+        System.out.println(c);
+
+
+    }
+}
